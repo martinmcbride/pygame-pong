f9382bc1a34cb4fd81d3b4d7426a643ca8f33640
Martin <mcbride.martin@gmail.com>
2022-09-01 17:17:39 +0100

    Step 3 completed

---
+++ step-3/main.py
@@ -0,0 +1,63 @@
+# Author:  Martin McBride
+# Created: 2022-08-28
+# Copyright (c) 2022, Martin McBride
+# License: MIT
+
+import pygame as pg
+from sprites import Ball, Bat
+
+class Game:
+    pass
+
+game = Game()
+
+# Setup game
+
+game.screen_width = 640
+game.screen_height = 480
+
+# Initialise pygame
+pg.init()
+
+# Set display size
+screen = pg.display.set_mode((game.screen_width, game.screen_height))
+
+# Create rectangles at boundaries of the screen
+
+game.left = pg.Rect(-100, 0, 100, game.screen_height)
+game.right = pg.Rect(game.screen_width, 0, 100, game.screen_height)
+game.top = pg.Rect(0, -100, game.screen_width, 100)
+game.bottom = pg.Rect(0, game.screen_height, game.screen_width, 100)
+
+# Set window title
+pg.display.set_caption('Pong')
+
+# Create sprites
+
+game.ball_sprite = Ball((100, 200), game)
+game.bat_sprite = Bat((200, 400), game)
+game.all_sprites = pg.sprite.RenderPlain()
+game.all_sprites.add(game.ball_sprite)
+game.all_sprites.add(game.bat_sprite)
+
+# Game loop
+
+game.running = True
+
+# Start the clock
+clock = pg.time.Clock()
+
+while game.running:
+
+    # Check all events in queue
+    for event in pg.event.get():
+
+        # If a quit event occurs, reset running flag
+        if event.type == pg.QUIT:
+            game.running = False
+
+    delta_time = clock.tick(60)
+    screen.fill((0, 0, 0))
+    game.all_sprites.update(delta_time)
+    game.all_sprites.draw(screen)
+    pg.display.flip()
---
+++ step-3/sprites.py
@@ -0,0 +1,45 @@
+# Author:  Martin McBride
+# Created: 2022-08-28
+# Copyright (c) 2022, Martin McBride
+# License: MIT
+
+import pygame as pg
+
+class Ball(pg.sprite.Sprite):
+
+    def __init__(self, pos, game):
+        super(Ball, self).__init__()
+        self.game = game
+        self.image = pg.image.load('ball.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+        self.speed = 0.2
+        self.direction = pg.Vector2(1, -1)
+
+    def update(self, delta_time):
+        self.rect.center += self.direction * self.speed * delta_time
+        screen_size = pg.display.get_window_size()
+        sprite_size = self.image.get_size()
+        if self.rect.colliderect(self.game.left):
+            self.direction[0] = 1
+        if self.rect.colliderect(self.game.right):
+            self.direction[0] = -1
+        if self.rect.colliderect(self.game.top):
+            self.direction[1] = 1
+        if self.rect.colliderect(self.game.bottom):
+            self.game.running = False
+        if self.rect.colliderect(self.game.bat_sprite.rect):
+            self.direction[1] = -1
+
+
+class Bat(pg.sprite.Sprite):
+
+    def __init__(self, pos, game):
+        super(Bat, self).__init__()
+        self.game = game
+        self.image = pg.image.load('bat.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+
+    def update(self, delta_time):
+        self.rect.center = (pg.mouse.get_pos()[0], self.rect.center[1])
